ec537ee3821e6d1b425c73900b05b7f72cd9bd76
artroot <art90com@gmail.com>
2017-04-18 20:18:11 +0300

    beta

---
+++ js/graph.js
@@ -0,0 +1,293 @@
+﻿function Graphic(data, defaultMode) {
+    if (data instanceof Object) {
+        this.mode = (defaultMode == null ? 'hour' : defaultMode);
+        this.autoLoad = true;
+
+        this.graphids = data.graphids;
+        this.port = data.port;
+        this.period = data.period;
+        this.stime = data.stime;
+        this.width = data.width;
+        this.height = data.height;
+
+        this.modes = [
+            {
+                'name': 'Час',
+                'mode': 'hour',
+                'period': 3600
+            },
+            {
+                'name': 'День',
+                'mode': 'day',
+                'period': 86400
+            },
+            {
+                'name': 'Месяц',
+                'mode': 'month',
+                'period': 2592000
+            }
+        ];
+        this.source = document.getElementById('graph_' + this.port);
+	this.source.className='zabbix-graphics';
+    } else Console.error('No data!');
+
+    this.getLnk = function (data) {
+        var lnk = 'zabbix';
+        data.forEach(function (item, i) {
+            lnk = lnk + (i == 0 ? '?' : '&');
+            lnk = lnk + item.name + '=' + item.value;
+        });
+        return lnk;
+    };
+
+    this.createControls = function () {
+        var self = this;
+        var controls = document.createElement('ul');
+        controls.className = 'pagination';
+        this.source.appendChild(controls);
+
+        var back = document.createElement('li');
+        var backLnk = document.createElement('a');
+        var backContent = document.createElement('span');
+
+        backLnk.href = 'javascript:void(0)';
+        backLnk.setAttribute('mode', this.mode);
+        backLnk.setAttribute('aria-label', 'Previous');
+        backContent.innerHTML = '&laquo;';
+
+        controls.appendChild(back);
+        back.appendChild(backLnk);
+        backLnk.appendChild(backContent);
+        backLnk.onclick = function () {
+            self.goBack(this);
+        };
+
+        var createdModesList = [];
+        this.modes.forEach(function (modeObj) {
+            var mode = document.createElement('li');
+            var modeLnk = document.createElement('a');
+            mode.className = (modeObj.mode == self.mode ? 'active' : '');
+            mode.setAttribute('period', modeObj.period);
+            mode.setAttribute('mode', modeObj.mode);
+            modeLnk.href = 'javascript:void(0)';
+            modeLnk.innerHTML = modeObj.name;
+            controls.appendChild(mode);
+            mode.appendChild(modeLnk);
+            mode.onclick = function () {
+                self.setMode(this);
+            };
+            createdModesList.push(mode);
+        });
+
+        var front = document.createElement('li');
+        var frontLnk = document.createElement('a');
+        var frontContent = document.createElement('span');
+
+        frontLnk.href = 'javascript:void(0)';
+        frontLnk.setAttribute('mode', this.mode);
+        frontLnk.setAttribute('aria-label', 'Next');
+        frontContent.innerHTML = '&raquo;';
+
+        controls.appendChild(front);
+        front.appendChild(frontLnk);
+        frontLnk.appendChild(frontContent);
+        frontLnk.onclick = function () {
+            self.goFront(this);
+        };
+        this.controlElements = {
+            'back': back,
+            'front': front,
+            'mods': createdModesList
+        };
+    };
+
+    this.createGraphic = function () {
+        self = this;
+        this.graphics = [];
+        var stime = new Date(this.stime);
+        this.graphids.forEach(function (item) {
+            var graphSpace = document.createElement('figure');
+            var graph = document.createElement('img');
+            graph.src = self.getLnk([
+                {'name': 'graphid', 'value': item.graphid},
+                {'name': 'period', 'value': self.period},
+                {'name': 'stime', 'value': stime.incDate(self.mode, 0).outNewDateForZabbixFormat()},
+                {'name': 'width', 'value': self.width},
+                {'name': 'height', 'value': self.height}
+            ]);
+            graph.setAttribute('graphid', item.graphid);
+            self.source.appendChild(graphSpace);
+            graphSpace.appendChild(graph);
+            self.graphics.push(graph);
+        });
+        this.stime = stime.outNewDate();
+    };
+
+    this.create = function () {
+        this.createControls();
+        var timer = document.createElement('p');
+        timer.className = 'timer';
+        timer.innerHTML = (this.autoLoad ? 30 : 0);
+        this.source.appendChild(timer);
+        this.createGraphic();
+        this.timer = timer;
+        setInterval(function () {
+            if (self.autoLoad) {
+                timer.innerHTML = timer.innerHTML - 1;
+            } else {
+                timer.innerHTML = 0;
+            }
+        }, 1000, timer);
+        setInterval(function () {
+            if (self.autoLoad) {
+                timer.innerHTML = 30;
+                var stime = new Date().decDate(self.mode, 1);
+                self.graphics.forEach(function (item) {
+                    item.setAttribute('src', self.getLnk([
+                        {'name': 'graphid', 'value': item.getAttribute('graphid')},
+                        {'name': 'period', 'value': self.period},
+                        {
+                            'name': 'stime', 'value': stime.getUTCFullYear() +
+                        ("0" + (stime.getUTCMonth() + 1)).slice(-2) +
+                        ("0" + stime.getUTCDate()).slice(-2) +
+                        ("0" + stime.getHours()).slice(-2) +
+                        ("0" + stime.getUTCMinutes()).slice(-2) +
+                        ("0" + stime.getUTCSeconds()).slice(-2)
+                        },
+                        {'name': 'width', 'value': self.width},
+                        {'name': 'height', 'value': self.height}
+                    ]))
+                });
+                self.stime = stime.getUTCFullYear() + '-' +
+                    ("0" + (stime.getUTCMonth() + 1)).slice(-2) + '-' +
+                    ("0" + stime.getUTCDate()).slice(-2) + 'T' +
+                    ("0" + stime.getHours()).slice(-2) + ':' +
+                    ("0" + stime.getUTCMinutes()).slice(-2) + ':' +
+                    ("0" + stime.getUTCSeconds()).slice(-2);
+            }
+        }, 30000, self, timer);
+    };
+
+    this.setMode = function (element) {
+        var self = this;
+        this.autoLoad = false;
+        this.mode = element.getAttribute('mode');
+        this.period = element.getAttribute('period');
+        this.controlElements.mods.forEach(function (item) {
+            item.className = '';
+        });
+
+        element.className = 'active';
+        this.controlElements.back.setAttribute('mode', this.mode);
+        this.controlElements.front.setAttribute('mode', this.mode);
+
+        var stime = new Date(this.stime);
+
+        this.graphics.forEach(function (item) {
+            item.setAttribute('src', self.getLnk([
+                {'name': 'graphid', 'value': item.getAttribute('graphid')},
+                {'name': 'period', 'value': self.period},
+                {'name': 'stime', 'value': stime.incDate(self.mode, 0).outNewDateForZabbixFormat()},
+                {'name': 'width', 'value': self.width},
+                {'name': 'height', 'value': self.height}
+            ]))
+        });
+
+        this.stime = stime.outNewDate();
+    };
+
+    this.goBack = function () {
+        var self = this;
+        var stime = new Date(this.stime);
+        this.autoLoad = false;
+
+        this.graphics.forEach(function (item) {
+            item.setAttribute('src', self.getLnk([
+                {'name': 'graphid', 'value': item.getAttribute('graphid')},
+                {'name': 'period', 'value': self.period},
+                {'name': 'stime', 'value': stime.decDate(self.mode, 1).outNewDateForZabbixFormat()},
+                {'name': 'width', 'value': self.width},
+                {'name': 'height', 'value': self.height}
+            ]))
+        });
+
+        this.stime = stime.outNewDate();
+    };
+
+    this.goFront = function () {
+        var self = this;
+        var stime = new Date(this.stime).incDate(self.mode, 1);
+        var stimeNow = new Date().decDate(this.mode, 1);
+
+        var s_stime = stime.getYear() + ("0" + (stime.getUTCMonth() + 1)).slice(-2) + ("0" + stime.getUTCDate()).slice(-2) + ("0" + stime.getUTCHours()).slice(-2) + ("0" + stime.getUTCMinutes()).slice(-2);
+        var s_stimeNow = stimeNow.getYear() + ("0" + (stimeNow.getUTCMonth() + 1)).slice(-2) + ("0" + stimeNow.getUTCDate()).slice(-2) + ("0" + stimeNow.getHours()).slice(-2) + ("0" + stimeNow.getUTCMinutes()).slice(-2);
+
+        if (s_stime >= s_stimeNow) {
+            this.timer.innerHTML = 30;
+            this.autoLoad = true;
+        } else {
+            this.timer.innerHTML = 0;
+            this.autoLoad = false;
+            this.graphics.forEach(function (item) {
+                item.setAttribute('src', self.getLnk([
+                    {'name': 'graphid', 'value': item.getAttribute('graphid')},
+                    {'name': 'period', 'value': self.period},
+                    {'name': 'stime', 'value': stime.outNewDateForZabbixFormat()},
+                    {'name': 'width', 'value': self.width},
+                    {'name': 'height', 'value': self.height}
+                ]))
+            });
+
+            this.stime = stime.outNewDate();
+        }
+    };
+
+    Date.prototype.decDate = function (mode, value) {
+        switch (mode) {
+            case 'hour':
+                this.setHours(this.getHours() - value);
+                break;
+            case 'day':
+                this.setDate(this.getDate() - value);
+                break;
+            case 'month':
+                this.setMonth(this.getMonth() - value);
+                break;
+        }
+        return this;
+    };
+
+    Date.prototype.incDate = function (mode, value) {
+        switch (mode) {
+            case 'hour':
+                this.setHours(this.getHours() + value);
+                break;
+            case 'day':
+                this.setDate(this.getDate() + value);
+                break;
+            case 'month':
+                this.setMonth(this.getMonth() + value);
+                break;
+        }
+        return this;
+    };
+
+    Date.prototype.outNewDateForZabbixFormat = function () {
+        return this.getUTCFullYear() +
+            ("0" + (this.getUTCMonth() + 1)).slice(-2) +
+            ("0" + this.getUTCDate()).slice(-2) +
+            ("0" + this.getUTCHours()).slice(-2) +
+            ("0" + this.getUTCMinutes()).slice(-2) +
+            ("0" + this.getUTCSeconds()).slice(-2);
+    };
+
+    Date.prototype.outNewDate = function () {
+        return this.getUTCFullYear() + '-' +
+            ("0" + (this.getUTCMonth() + 1)).slice(-2) + '-' +
+            ("0" + this.getUTCDate()).slice(-2) + 'T' +
+            ("0" + this.getUTCHours()).slice(-2) + ':' +
+            ("0" + this.getUTCMinutes()).slice(-2) + ':' +
+            ("0" + this.getUTCSeconds()).slice(-2);
+    };
+
+}
